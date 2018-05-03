9b35ffef0176833a09054455fac10ccf0712f18b
chenchen5976 <38742582+chenchen5976@users.noreply.github.com>
2018-05-03 17:40:17 +0800

    1.1.86
    搜索为空组件样式调整

--- components/Icon/NoContent.vue
+++ components/Icon/NoContent.vue
@@ -1,5 +1,8 @@
 <template>
-	<div><img src="../../static/images/bg/bt_noContent.png"/></div>
+	<div class="nocontent">
+		<img src="../../static/images/bg/bt_noContent.png"/>
+		<p>小呱没有找到相关内容喔~</p>
+	</div>
 </template>
 
 <script>
@@ -18,5 +21,16 @@
 	}
 </script>
 
-<style>
+<style scoped>
+	.nocontent{
+		text-align: center;
+		padding: 20px 10px;
+	}
+	.nocontent img{
+		width: 80px;
+	}
+	.nocontent p{
+		padding-bottom: 0px;
+		margin-bottom: 0px;
+	}
 </style>

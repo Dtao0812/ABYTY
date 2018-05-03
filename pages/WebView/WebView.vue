a85960ea542935a23970901de2d981e5392ff202
chenchen5976 <38742582+chenchen5976@users.noreply.github.com>
2018-05-03 16:13:37 +0800

    1.1.76
    订单详情页面底部按钮样式

--- pages/WebView/WebView.vue
+++ pages/WebView/WebView.vue
@@ -4,13 +4,17 @@
 		<div class="mui-content" slot="content">
 			<iframe id="myiframe" ref="iframe" :src="webUrl" width="100%" name="myiframe" height="100%"></iframe>
 			<div class="operation" v-if="cpBasic != ''">
-				<div class="mui-col-xs-6" @click="toChat">
-					<aby-icon class="mui-icon" type="chat"></aby-icon><span class="icotext">聊天</span>
+				<div class="mui-col-xs-3" @click="toChat">
+					<aby-icon class="mui-icon" type="msg"></aby-icon><span class="icotext">聊天</span>
 				</div>
-				<div class="mui-col-xs-6" @click="toHomePage">
-					<aby-icon class="mui-icon" type="chat"></aby-icon><span class="icotext">主页</span>
+				<div class="mui-col-xs-3" @click="toHomePage">
+					<aby-icon class="mui-icon" type="business"></aby-icon><span class="icotext">主页</span>
+				</div>
+				<div class="mui-col-xs-6 btndisabled">
+					<input type="button" value="立即预订" />
 				</div>
 			</div>
+
 		</div>
 	</aby-page>
 </template>
@@ -18,7 +22,7 @@
 	.mui-content {
 		height: 100%;
 		overflow-x: hidden;
-		overflow-y: auto; 
+		overflow-y: auto;
 	}
 	
 	iframe {
@@ -26,8 +30,9 @@
 		height: 100%;
 		width: 100%;
 		overflow-x: hidden;
-		overflow-y: auto; 
+		overflow-y: auto;
 	}
+	
 	.operation {
 		height: 45px;
 		width: 100%;
@@ -36,8 +41,10 @@
 		background-color: #FFFFFF;
 		text-align: center;
 		font-size: 14px;
+		border-top: 1px solid #F0F0F0;
 	}
-	.operation .mui-col-xs-6{
+	
+	.operation .mui-col-xs-3,.operation .mui-col-xs-6 {
 		float: left;
 		padding: 5px 0px;
 	}
@@ -47,13 +54,21 @@
 	}
 	
 	.operation .icotext {
-		display: block;
 		font-size: 12px;
 		margin-top: -5px;
+		display: block;
 	}
-	
-	
-	
+	.btndisabled{
+		background-color: #F0F0F0;
+		color: #FFFFFF;
+		height: 100%;
+	}
+	.btndisabled input{
+		border: none;
+		background-color: rgba(255,255,255,0);
+		color: #DDDDDD;
+		line-height: 25px;
+	}
 </style>
 <script>
 	export default {
@@ -62,8 +77,8 @@
 				loading: false,
 				webUrl: this.$route.params.url,
 				webTitle: this.$route.params.title || '',
-				cpId:this.$route.params.cpId||'',
-				cpBasic:'',
+				cpId: this.$route.params.cpId || '',
+				cpBasic: '',
 			}
 		},
 		mounted() {
@@ -71,34 +86,36 @@
 			this.getCpBasic();
 		},
 		methods: {
-			init(){
+			init() {
 				this.webUrl = this.$route.params.url
 				this.webTitle = this.$route.params.title || ''
-				this.cpId =this.$route.params.cpId||''
-				
+				this.cpId = this.$route.params.cpId || ''
+
 				this.getCpBasic();
 			},
 			// 获得用户信息
-			getCpBasic(){
+			getCpBasic() {
 				let reqInfo = {};
 				reqInfo.cpId = this.cpId;
-				this.$abyApi.User.getBasicNotLegal(reqInfo,(res)=>{
+				this.$abyApi.User.getBasicNotLegal(reqInfo, (res) => {
 					this.cpBasic = res.cpBasic
 				})
 			},
 			// 聊天
-			toChat(){
+			toChat() {
 				this.$router.push({
 					name: 'chat',
 					params: {
-						userId:this.cpBasic.userId
+						userId: this.cpBasic.userId
 					}
 				});
 			},
 			// 公司主页
-			toHomePage(){
+			toHomePage() {
 				this.$store.commit('setHomePage', this.cpBasic);
-				this.$router.push({name:"homePage"})
+				this.$router.push({
+					name: "homePage"
+				})
 			}
 		},
 		watch: {

1b14b88da0f7a9f5e14a99e29a74d96d7e4d2151
Dtao0812 <36064874+Dtao0812@users.noreply.github.com>
2018-05-23 15:31:28 +0800

    1.1.201
    特价路线图片高度不一致

--- components/List/CardLine.vue
+++ components/List/CardLine.vue
@@ -5,7 +5,7 @@
 		<div class="mui-card aby-special" v-for="(li,i) in list" :key="i" @click="toDetail(li)">
 			<div class="mui-card-content">
 				<img v-if="li.isClear == 1" class="faceTip" src="../../static/images/ico/ico_sale.png" />
-				<img class="face" :src="li.proImage" />
+				<img class="face" :src="li.proImage" :style="{ height: imgHeight }"/>
 				<h4 class="aby-list-title-2">【{{li.mTitle}}】{{li.sTitle}}</h4>
 				<p class="aby-list-company mui-ellipsis"><aby-icon type="business" class="icobusiness"></aby-icon>{{li.cpName}}</p>
 				<p class="aby-list-data mui-ellipsis"><span v-if="li.groupType != '不限'" v-text="li.groupType"></span><span>浏览({{li.readCnt}})</span></p>
@@ -25,7 +25,8 @@
 		props: ['list', 'page', 'proState'],
 		data() {
 			return {
-				noContent:false
+				noContent:false,
+				imgHeight: 0
 			}
 		},
 		methods: {
@@ -41,23 +42,19 @@
 					}
 				});
 			},
-//			setImgSize(){
-//				console.log(22)
-//				var imgArr = document.getElementsByClassName("face");
-//				console.log(imgArr)
-//				for(var i = 0; i < imgArr.length; i++) {
-//					imgArr[i].style.height = 2*imgArr[i].clientWidth / 3 + "px";
-//				}
-//			}
+			setImgHeight(){
+				let imgArr = document.getElementsByClassName("face");
+				this.imgHeight = 2*imgArr[0].clientWidth / 3 + "px";
+			}
 		},
 		mounted() {
-//			this.setImgSize();
+			this.setImgHeight();
 		},
 		watch: {  
 		    list(val){
 		    	this.noContent = val.length == 0;
 		    	this.list = val;
-//		    	this.setImgSize();
+		    	
 		    }  
 		}  
 	}
@@ -65,7 +62,7 @@
 
 <style scoped>
 	.card-item{
-		min-height: 800px;
+		/*min-height: 800px;*/
 	}
 	.icobusiness {
 		font-size: 12px;
